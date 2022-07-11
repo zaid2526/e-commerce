ad52b239835494aa982ccdb1882cb93cc1ed20fe
zaid2526 <mzr16894@.com>
2022-07-11 17:08:02 +0530

    task6(image-crousel)

---
+++ image-crousel/image-crousel.js
@@ -0,0 +1,21 @@
+const imgs=document.getElementById('imgs');
+
+const img=document.querySelectorAll('#imgs img')
+
+
+let idx=0;
+
+function run(){
+    idx++;
+    if(idx>img.length-1){
+        idx=0;
+    }
+    imgs.style.transform=`translateX( ${-idx * 500}px)`
+    console.log('a');
+
+    
+}
+
+setInterval(()=>{
+    run();
+},2000)
